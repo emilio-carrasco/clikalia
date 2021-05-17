bb8496662a13fac2af589000f9af8ac44da45e11
John Doe <emilioemail@gmail.com>
2021-05-17 13:34:23 +0200

    ma´s ó menos fucional y con barras

--- src/display.py
+++ src/display.py
@@ -3,13 +3,15 @@ from PIL import Image
 
 import pandas as pd
 import src.utilidades as ut
-import src.datos as datos
+import config as conf
 import json
+import matplotlib.pyplot as plt
+
 def header():
     st.set_page_config(page_title="RECOMENDADOR CLIKALIA", page_icon='./img/clikalia_icono.png', layout='wide', initial_sidebar_state= 'expanded')
     #st.sidebar.text(ut.hora())
     imagen = Image.open("img/clikalia_logo.png")
-    st.image(imagen, width=300)
+    st.image(imagen, width=150)
     
 def menu_seleccion():
     return st.sidebar.selectbox("MENÜ",("PRINCIPAL","DATOS"))
@@ -29,8 +31,7 @@ def caja_precio_m2(id):
         for _,r in df.iterrows():
             diccionario[int(r[0])] = float(r[1])
         with open("./config/preciom2.json", "w") as archivojson: 
-            json.dump(diccionario, archivojson) 
-        
+            json.dump(diccionario, archivojson)       
 
 def caja_resultados_busqueda(df):
     if df is None: 
@@ -40,13 +41,16 @@ def caja_resultados_busqueda(df):
             df_show = df.set_index('uv') #2dohay que arreglar
         except: 
             df_show = df
-        colum_maysc = [c for c in df.columns if c.lower() != c]
+        #colum_maysc = [c for c in df.columns if c.lower() != c]
+        #colum_maysc = [k for k,v in filtro.items() if v]
+        colum_maysc = df.columns #2do esto se podría quitar
         st.dataframe(df_show[colum_maysc])
         
 def bonito_df_busqueda(df):
     try:
         claves = df.columns
-        claves = [c for c in claves]# if c.lower() != c]
+        claves = [c for c in claves if c.lower() != c]
+        
         df =  df.applymap(str)[claves] 
         return df.reset_index(level=0)
     except: return None
@@ -54,7 +58,7 @@ def bonito_df_busqueda(df):
 def caja_filtros(filtro):
     "# FILTROS:"  
     l = len(filtro)
-    num_max_columnas = 6
+    num_max_columnas = conf.max_filtros
     num_columnas = (l if l <= num_max_columnas else num_max_columnas)
     cols = st.beta_columns(num_columnas)
     for i, (k,v) in enumerate(filtro.items()):
@@ -64,10 +68,33 @@ def caja_filtros(filtro):
 def caja_boton_busqueda(df):
     busqueda = st.sidebar.text_input("Buscar:", "")
     if busqueda: 
-        try:
+        try:     
+            df = df.reset_index()
             busqueda = busqueda.split(' ')
             df = df[df.apply(lambda x: ut.buscar(busqueda,list(x)), axis=1 )]
             boton_busqueda=False
+            df = df.set_index('uv')
         except:
             "No se puede ejecuatar la búsqueda"
     return df
+
+def barras_pesos(pesos):
+    s  = pd.Series(pesos,index=pesos.keys())
+    s.sort_values(ascending= False, inplace= True)
+    x_values = s.index .str.upper()
+    y_values = s.values
+    fig = plt.figure(figsize = (20, 2))
+    plt.style.use('dark_background')
+    plt.xticks(rotation=90)
+    plt.bar(x_values, y_values, color='pink')
+    
+    st.pyplot(fig)
+    
+
+def selector_pesos():
+    st.sidebar.selectbox("PERFILES",("FAMILIA","PAREJA JOVEN", "SOLTERO"),key='perfiles')
+    diccionario_pesos=dict()
+    pesos= conf.pesos_finales
+    for p in pesos:
+        diccionario_pesos[p]=st.sidebar.slider(p.upper(), 0, 100,50,key=p)/100
+    return diccionario_pesos
